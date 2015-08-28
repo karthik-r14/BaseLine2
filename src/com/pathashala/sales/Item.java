100dbf30729927f7c82d76efb3fc57ad1cb04498
karthikr-14 <karthik14081993@gmail.com>
2015-08-28 18:39:21 +0530

    Modified Build.xml, added a spec and created a need for a class

---
+++ src/com/pathashala/sales/Item.java
@@ -0,0 +1,13 @@
+package com.pathashala.sales;
+
+public class Item {
+    String itemName;
+
+    public Item(String item) {
+        itemName = item;
+    }
+
+    public double computeSalesTax() {
+        return 0.0;
+    }
+}
---
+++ test/com/pathashala/sales/TestSales.java
@@ -0,0 +1,15 @@
+package com.pathashala.sales;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestSales {
+
+    @Test
+    public void shouldHaveSalesTaxZeroIfItemIsAMedicalProduct() {
+        Item item = new Item("Headache pills");
+
+        assertEquals(0.0, item.computeSalesTax(), 0.001);
+    }
+}
